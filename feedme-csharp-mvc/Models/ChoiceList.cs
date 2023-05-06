15947c2f46fbcfa9f2ddf6c7a7167354955da08a
Your Name <108307350+pjduri@users.noreply.github.com>
2023-05-06 13:09:56 -0500

    Add project files.

---
+++ feedme-csharp-mvc/Models/ChoiceList.cs
@@ -0,0 +1,37 @@
+﻿namespace feedme_csharp_mvc.Models
+{
+    public class ChoiceList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Option> Options { get; set; }
+
+        public ChoiceList()
+        {
+        }
+
+        public ChoiceList(string name, string description, List<Option> options)
+        {
+            Name = name;
+            Description = description;
+            Options = options;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ChoiceList list &&
+                   Id == list.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+    }
+}
